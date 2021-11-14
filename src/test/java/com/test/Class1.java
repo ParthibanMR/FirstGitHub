7deb9cbf8d181786e2e93aedb8a0a16aef82dde0
ParthibanMR <parthibanravi.m.r@gmail.com>
2021-11-14 09:33:06 +0530

    Fisrt Time Pushing the code

---
+++ src/test/java/com/test/Class1.java
@@ -0,0 +1,17 @@
+package com.test;
+
+public class Class1 {
+	private void mehod1() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method2() {
+		// TODO Auto-generated method stub
+
+	}
+	private void method3() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
